0ad553b103ca9195ffd9845dd61156691fe33a1c
Quborg <miloud.laftah@gmail.com>
2015-08-22 19:55:18 +0100

    wrap duplicated components, and use props on it.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 var Component = React.createClass({
 	render: function() {
-		return <div>My React component.</div>;
+		return <div>
+					<h1>{this.props.text}</h1>
+					<p>{this.props.children}</p>
+			</div>;
 	}
 });
-React.render(<Component/>,document.getElementById('react-container'));
+React.render(	<div>
+					<Component text="Hello world">This is a hello world</Component>
+					<Component text="How are you ?">This is a how are you ?</Component>
+					<Component text="Goodbye">This is a goodbye</Component>
+				</div>
+	,document.getElementById('react-container')
+);
