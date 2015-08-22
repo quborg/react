fbaf26b9ee8d5bc621ce852e6bb714156281e364
Quborg <miloud.laftah@gmail.com>
2015-08-23 00:09:30 +0100

    checkbox state

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
-var Component = React.createClass({
+var Checkbox = React.createClass({
+	getInitialState: function() {
+		return {checked: true}
+	},
+	handleCheck: function() {
+		this.setState({checked: !this.state.checked})
+	},
 	render: function() {
-		return <div>My React component.</div>;
+		var msg;
+		if (this.state.checked) msg = "checked";
+		else msg = "unchecked";
+		return (<div>
+					<input type="checkbox" onChange={this.handleCheck} defaultChecked={this.state.checked}/>
+					<p>This box is {msg}</p>
+				</div>
+		);
 	}
 });
-React.render(<Component/>,document.getElementById('react-container'));
+React.render(<Checkbox/>,document.getElementById('react-container'));
