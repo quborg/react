dbf1905d35fcd3d8dc2587b3e9b0a3c79d153f94
Quborg <miloud.laftah@gmail.com>
2015-08-26 22:49:02 +0100

    use this.style and create a random position for new note

--- js/Note.js
+++ js/Note.js
@@ -2,6 +2,16 @@ var Note = React.createClass({
 	getInitialState: function() {
 		return {editing: false}
 	},
+	componentWillMount: function() {
+		this.style = {
+			right: this.randomBetween(0, window.innerWidth - 150) + 'px',
+			top: this.randomBetween(0, window.innerHeight - 150) + 'px',
+			transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
+		};
+	},
+	randomBetween: function(min, max) {
+		return (min + Math.ceil(Math.random() * max));
+	},
 	edit: function() {
 		this.setState({editing: true});
 	},
@@ -20,7 +30,7 @@ var Note = React.createClass({
 	},
 	renderForm: function() {
 		return (
-			<div className="note">
+			<div className="note" style={this.style}>
 				<textarea ref="newText" defaultValue={this.props.children} className="form-control"></textarea>
 				<button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
 			</div>
@@ -28,7 +38,7 @@ var Note = React.createClass({
 	},
 	renderDisplay: function() {
 		return (
-			<div className="note">
+			<div className="note" style={this.style}>
 				<p>{this.props.children}</p>
 				<span>
 					<button onClick={this.edit} className="btn btn-primary glyphicon glyphicon-pencil"/>
@@ -51,14 +61,21 @@ var Board = React.createClass({
 	getInitialState: function() {
 		return { notes: [] };
 	},
+	nextId: function() {
+		this.uniqueId = this.uniqueId || 0;
+		return this.uniqueId++;
+	},
 	add: function(text) {
 		var arr = this.state.notes;
-		arr.push(text);
+		arr.push({
+			id: this.nextId(),
+			note: text,
+		});
 		this.setState({notes:arr});
 	},
 	update: function(newText, i) {
 		var arr = this.state.notes;
-		arr[i] = newText;
+		arr[i].note = newText;
 		this.setState({notes:arr});
 	},
 	remove: function(i) {
@@ -68,14 +85,14 @@ var Board = React.createClass({
 	},
 	eachNote: function(note, i) {
 		return (
-			<Note key={i} index={i} onChange={this.update} onRemove={this.remove}>{note}</Note>
+			<Note key={note.id} index={i} onChange={this.update} onRemove={this.remove}>{note.note}</Note>
 		);
 	},
 	render: function() {
 		return (
 			<div className="board">
 				{this.state.notes.map(this.eachNote)}
-				<button className="btn btn-sm btn-success glyphicon glyphicon-save-file" onClick={this.add.bind(null, "New Note")} />
+				<button id="button" className="btn btn-sm btn-success glyphicon glyphicon-save-file" onClick={this.add.bind(null, "New Note")} />
 			</div>
 		);
 	},
