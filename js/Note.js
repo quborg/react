82df8f5c1b5a1e2c5a80141564a91cf637b1f08e
Quborg <miloud.laftah@gmail.com>
2015-08-30 12:31:53 +0100

    getJSON data and generate component with props combination

--- js/Note.js
+++ js/Note.js
@@ -9,6 +9,9 @@ var Note = React.createClass({
 			transform: 'rotate(' + this.randomBetween(-15, 15) + 'deg)'
 		};
 	},
+	componentDidMount: function() {
+		$(this.getDOMNode()).draggable();
+	},
 	randomBetween: function(min, max) {
 		return (min + Math.ceil(Math.random() * max));
 	},
@@ -64,6 +67,17 @@ var Board = React.createClass({
 	nextId: function() {
 		this.uniqueId = this.uniqueId || 0;
 		return this.uniqueId++;
+	},
+	componentWillMount: function() {
+		var self = this;
+		if(this.props.count) {
+			$.getJSON("http://baconipsum.com/api/?type=all-meat&sentences=" +
+				this.props.count + "&start-with-lorem=1&callback=?", function(results){
+					results[0].split('. ').forEach(function(sentence){
+						self.add(sentence.substring(0,40));
+					});
+				});
+		}
 	},
 	add: function(text) {
 		var arr = this.state.notes;
